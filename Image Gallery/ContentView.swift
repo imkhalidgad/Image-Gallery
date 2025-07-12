0651ba1244d042eb6139643cf9f5f1d616f962cc
Khalid Gad <107983594+imkhalidgad@users.noreply.github.com>
2025-07-12 21:13:50 +0300

    Initial Commit

---
+++ Image Gallery/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Image Gallery
+//
+//  Created by Khalid Gad on 12/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Image Gallery/Image_GalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_GalleryApp.swift
+//  Image Gallery
+//
+//  Created by Khalid Gad on 12/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct Image_GalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
